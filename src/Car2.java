2403958c66caa697452b7aeba0e2b4d82140af1b
Evilous10101 <michael.lannon@yahoo.com>
2018-09-13 10:20:27 -0400

    first commit

---
+++ src/Car2.java
@@ -0,0 +1,13 @@
+
+public class Car2 
+{
+private String Car;
+
+public String getcar() {
+	return Car;
+}
+public void setCar(String Car) {
+	this.Car = Car;
+}
+	
+}
---
+++ src/app.java
@@ -0,0 +1,15 @@
+
+public class app {
+
+	public static void main(String[] args){
+	
+		Car2 u1 = new Car2();
+		u1.setCar("mitibutchi");
+		System.out.println(u1.getcar());
+}
+	{
+	
+		for (int x = 0; x < 100; x++)
+		System.out.println(x);
+	}
+	}
